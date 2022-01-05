afca502b09b08ff434dde9ff170fcbb912c304f8
yurigarrido <y.garrido.yuri@gmail.com>
2022-01-05 11:56:13 -0300

    initial commit

--- desafio2.sql
+++
@@ -1,5 +0,0 @@
-CREATE VIEW estatisticas_musicais AS 
-SELECT COUNT(nome_cancao) AS 'cancoes',
-(SELECT COUNT(name_artista) FROM SpotifyClone.artistas) AS 'artistas',
-(SELECT COUNT(nome_album) FROM SpotifyClone.albuns) AS 'albuns'
-FROM SpotifyClone.cancoes;
